30b07f6aaa437db37c333e830d08917d1be588e7
David Tubridy <davidtubridy@Davids-MacBook-Pro.local>
2025-11-17 01:42:30 +0000

    eiq-003 create survey

--- libs/surveys/survey-list/src/lib/survey-toolbar/survey-toolbar.ts
+++ libs/surveys/survey-list/src/lib/survey-toolbar/survey-toolbar.ts
@@ -1,4 +1,14 @@
-import { Component, ChangeDetectionStrategy, output } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  output,
+  inject,
+  DestroyRef,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { SurveyService } from '@eiq/data-access';
+import { Survey, QuestionType } from '@eiq/models';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'eiq-survey-toolbar',
@@ -8,9 +18,11 @@ import { Component, ChangeDetectionStrategy, output } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SurveyToolbar {
+  private readonly surveyService = inject(SurveyService);
+  private readonly destroyRef = inject(DestroyRef);
+
   public surveyFilter = output<string>();
   public filter = output<void>();
-  public createSurvey = output<void>();
 
   protected searchQuery = '';
 
@@ -18,5 +30,38 @@ export class SurveyToolbar {
     const target = event.target as HTMLInputElement;
     this.searchQuery = target.value;
     this.surveyFilter.emit(this.searchQuery);
+  }
+
+  protected createSurvey() {
+    const newSurvey: Omit<Survey, 'id'> = {
+      title: 'Untitled Survey',
+      description: 'Description',
+      questions: [
+        {
+          questionId: 1,
+          questionText: 'Question 1',
+          mandatoryInd: false,
+          questionType: QuestionType.SingleChoice,
+          options: ['Option 1', 'Option 2'],
+          randomizeOptionsInd: false,
+          cards: [],
+          programmerNotes: '',
+          instructions: '',
+        },
+      ],
+    };
+
+    this.surveyService
+      .createSurvey(newSurvey)
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        catchError((err) => {
+          return throwError(() => err);
+        })
+      )
+      .subscribe(() =>
+        //TODO get the latest surveys
+        window.location.reload()
+      );
   }
 }
